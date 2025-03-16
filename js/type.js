7242d34bb82d9d075f05392985af7e3d6f71dfcc
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-03-16 09:28:25 +0200

    Add files via upload

---
+++ js/type.js
@@ -0,0 +1,79 @@
+// const secondsInMinute = 60;
+// const minutesInHours = 60;
+// const secondsInHours = secondsInMinute * minutesInHours;
+// const hoursInDay = 24;
+// const secondsInDay = secondsInHours * hoursInDay;
+// console.log(secondsInDay);
+
+// console.log(secondsInHours);
+
+// let balloons = 100;
+// balloons *= 2;
+// console.log(balloons);
+
+// let balloon = 100;
+// balloon /= 4;
+// console.log(balloon);
+
+// const weekend = false;
+// const shower = true;
+// const apple = true;
+// const orange = false;
+// const result = !weekend && shower && (apple || orange);
+// console.log(result);
+
+// const height = 60;
+// const heightNeed = 60;
+// const result = height > heightNeed;
+// console.log(result);
+
+// false
+
+// const height = 60;
+// const heightNeed = 60;
+// const result = height >= heightNeed;
+// console.log(result);
+
+// true
+
+// const height = 60;
+// const moreHeightNeed = 48;
+// const result = height < moreHeightNeed;
+// console.log(result);
+
+// false
+
+// const height = 48;
+// const moreHeightNeed = 48;
+// const result = height <= moreHeightNeed;
+// console.log(result);
+
+// true
+
+// const myNumber = 5;
+// const hisNumber = 3;
+// const hisResult = myNumber === hisNumber;
+// console.log(hisResult);
+
+// const herNumber = 5;
+// const herResult = herNumber === myNumber;
+// console.log(herResult);
+
+// const string = "5";
+// const number = 5;
+// const result = string == number;
+// console.log(result);
+
+//true
+
+// const string = "5";
+// const number = 5;
+// const result = string === number;
+// console.log(result);
+
+// false
+
+// const age = 13;
+// const accompained = false;
+// const result = age && !accompained;
+// console.log(result);
