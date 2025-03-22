a5246a87a0cee537635ec6859604dc850e31228d
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-03-22 15:02:22 +0200

    next

--- js/array.js
+++ js/array.js
@@ -6,19 +6,34 @@
 // const maniacs = ["Yakoo", "Google", "Microsoft"];
 // console.log(maniacs [maniacs.length - 1]);
 
-let animals = [];
-animals.push("Cat");
-animals.push("Dog");
-animals.push("Llama");
-animals.unshift("Polar Bear");
-animals.unshift("Monkey");
-console.log(animals);
+// let animals = [];
+// animals.push("Cat");
+// animals.push("Dog");
+// animals.push("Llama");
+// animals.unshift("Polar Bear");
+// animals.unshift("Monkey");
+// console.log(animals);
 
-const lastAnimals = animals.pop();
-animals.pop();
-animals.unshift(lastAnimals);
-console.log(lastAnimals);
-console.log(animals);
-const firstAnimal = animals.shift();
-console.log(firstAnimal);
-console.log(animals);
+// const lastAnimals = animals.pop();
+// animals.pop();
+// animals.unshift(lastAnimals);
+// console.log(lastAnimals);
+// console.log(animals);
+// const firstAnimal = animals.shift();
+// console.log(firstAnimal);
+// console.log(animals);
+
+// const on = ["Lev", "Bors"];
+// const ona = ["Vladka", "Nikolo"];
+// const ono = ["Nikoly", "Borsh"]
+// const all = on.concat(ona, ono);
+// console.log(all);
+
+// const myBook = ["Sherlok", "The book of Bill"];
+// const friendBook = ["LOx", "OOH"];
+// const allBook = myBook.concat(friendBook);
+// console.log(allBook);
+
+const animals = ["Monkey", "april", "Fish"];
+const result = animals.join(" , ");
+console.log(result);
