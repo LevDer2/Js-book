9dba14bff013efac85b02f76e3786bec49ad1cd8
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-03-23 14:31:14 +0200

    yes

--- js/array.js
+++ js/array.js
@@ -34,6 +34,28 @@
 // const allBook = myBook.concat(friendBook);
 // console.log(allBook);
 
-const animals = ["Monkey", "april", "Fish"];
-const result = animals.join(" , ");
-console.log(result);
+// const animals = ["Monkey", "april", "Fish"];
+// const result = animals.join(" lEV ");
+// console.log(result);
+
+// const ok = Math.floor(Math.random()*4);
+// console.log(ok);
+
+// const ran = ["Cheese", "Vegetables", "Eggs"];
+// const rand = Math.floor(Math.random(ran) * 3);
+// ran[rand];
+// console.log(rand);
+
+// const phareses = ["Yes", "No", "Maybe"];
+// const a = phareses [Math.floor(Math.random() * 3)];
+// console.log(`I need to go to school?`);
+// console.log(a);
+
+// const randomBody = ["Face", "Nose", "Hair"];
+// const randomAdjectives = ["Smelly", "Boring", "Stupid"];
+// const randomWords = ["Fly", "Marmot", "Stick", "Rat"];
+// const randomBo = randomBody[Math.floor(Math.floor(Math.random() * randomBody.length))]
+// const randomAd = randomAdjectives[Math.floor(Math.floor(Math.random() * randomAdjectives.length))]
+// const randomWo = randomWords[Math.floor(Math.floor(Math.random() * randomWords.length))];
+// const result = `Your ${randomBo} is like a ${randomAd} ${randomWo}`;
+// console.log(result);
